a2fef2cb82d5316943f1732507e78117c19772eb
mySkey <476168710@qq.com>
2018-08-31 11:18:55 +0800

    测试redux状态管理使用成功没，可在login页获取到状态改变后的值

--- src/page/login/Login.js
+++ src/page/login/Login.js
@@ -1,11 +1,23 @@
 import React,{ Component } from 'react'
 
-
+import { store } from '../../store/store.js'
 export default class Login extends Component {
+  constructor(props){
+    super(props)
+    this.state={
+      num:''
+    }
+  }
+  componentDidMount(){
+    console.log(store.counter.getState())
+    this.setState({
+      num: store.counter.getState() 
+    })
+  }
   render() {
     return (
       <div>
-        login 
+        login 现在计数为{this.state.num}
       </div>
     )
   }
