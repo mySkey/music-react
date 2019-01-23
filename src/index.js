fd77190b23078981e92eb9f16cdb3f36127d19b5
mySkey <476168710@qq.com>
2019-01-23 18:01:17 +0800

    使用react-redux

--- src/index.js
+++ src/index.js
@@ -5,8 +5,15 @@ import './index.css';
 import '@/common/ajax.js';
 import '@/common/common.js';
 
+import { Provider } from 'react-redux';
+import store from './store/store.js'
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
